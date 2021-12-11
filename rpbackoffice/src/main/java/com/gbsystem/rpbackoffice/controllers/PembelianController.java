baaf8fd1042477e76ac529205bc1c1425943acca
bagindaisfa <bagindaisfa05@gmail.com>
2021-12-11 22:47:25 +0700

    add Upload Pembelian

--- rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/controllers/PembelianController.java
+++ rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/controllers/PembelianController.java
@@ -1,17 +1,16 @@
 package com.gbsystem.rpbackoffice.controllers;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import com.gbsystem.rpbackoffice.entities.Pembelian;
+import com.gbsystem.rpbackoffice.repository.PembelianRepository;
 import com.gbsystem.rpbackoffice.services.PembelianService;
+
 
 @RestController
 @RequestMapping("/api")
@@ -31,13 +32,16 @@ public class PembelianController {
 	
 	@Autowired
 	private PembelianService pembelianService;
+	
+	@Autowired
+	private PembelianRepository eRepo;
 
     @GetMapping("/pembelian")
 	public ResponseEntity<List<Pembelian>> getAll() {
         return new ResponseEntity<>(pembelianService.getAllPembelian(), HttpStatus.OK);
     }
     
-    @PostMapping(value = "/addPembelian", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/addPembelian", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public @ResponseBody String saveProduct(@RequestParam("image") MultipartFile image,@RequestParam("artikel") String artikel,
     		@RequestParam("kategori") String kategori,@RequestParam("tipe") String tipe,
     		@RequestParam("nama_barang") String nama_barang,@RequestParam("kuantitas") double kuantitas,@RequestParam("ukuran") String ukuran,
@@ -49,6 +53,33 @@ public class PembelianController {
     	return "Insert Data Failed!";
 		
     }
+    
+    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void mapReapExcelDatatoDB(@RequestParam("file") MultipartFile readExcelDataFile) throws Exception {
+    	
+        XSSFWorkbook workbook = new XSSFWorkbook(readExcelDataFile.getInputStream());
+        XSSFSheet worksheet = workbook.getSheetAt(0);
+        
+        for(int i=1;i<worksheet.getPhysicalNumberOfRows() ;i++) {
+            Pembelian pembelian = new Pembelian();
+        	XSSFRow row = worksheet.getRow(i);
+        	pembelian.setImage(null);
+    		pembelian.setArtikel(row.getCell(0).getStringCellValue());
+    		pembelian.setKategori(row.getCell(4).getStringCellValue());
+			pembelian.setTipe(row.getCell(7).getStringCellValue());
+			pembelian.setNama_barang(row.getCell(6).getStringCellValue());
+			pembelian.setKuantitas(row.getCell(5).getNumericCellValue());
+			pembelian.setUkuran(row.getCell(8).getStringCellValue());
+			pembelian.setHpp(row.getCell(1).getNumericCellValue());
+    		pembelian.setHarga_jual(row.getCell(2).getNumericCellValue());
+    		pembelian.setTotal_hpp((row.getCell(5).getNumericCellValue()) * (row.getCell(1).getNumericCellValue()));
+    		pembelian.setRowstatus(1);
+    		pembelian.setTanggal_transaksi(new Date());
+    		eRepo.save(pembelian);
+        }
+        
+    }
+    
     @GetMapping("/deletePembelian/{id}")
     public String deletePembelian(@PathVariable("id") Long id)
     {
