cc2abf20c5de559cba549580d499e5e510c09515
bagindaisfa <bagindaisfa05@gmail.com>
2021-12-13 17:43:41 +0700

    Penyimpanan checkpoint

---
+++ rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/entities/PenyimpananKeluar.java
@@ -0,0 +1,142 @@
+package com.gbsystem.rpbackoffice.entities;
+
+import java.util.Date;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class PenyimpananKeluar {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@JsonFormat(pattern="yyyy MMMM dd")
+	private Date tanggal_transaksi;
+	private Long id_store;
+	private String lokasi_store;
+	private String artikel;
+	private String kategori;
+	private String tipe;
+	private String nama_barang;
+	private double kuantitas;
+	private String ukuran;
+	private double hpp;
+	private double harga_jual;
+	private double total_hpp;
+	private String keterangan;
+	private int rowstatus;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getTanggal_transaksi() {
+		return tanggal_transaksi;
+	}
+	public void setTanggal_transaksi(Date tanggal_transaksi) {
+		this.tanggal_transaksi = tanggal_transaksi;
+	}
+	public Long getId_store() {
+		return id_store;
+	}
+	public void setId_store(Long id_store) {
+		this.id_store = id_store;
+	}
+	public String getLokasi_store() {
+		return lokasi_store;
+	}
+	public void setLokasi_store(String lokasi_store) {
+		this.lokasi_store = lokasi_store;
+	}
+	public String getArtikel() {
+		return artikel;
+	}
+	public void setArtikel(String artikel) {
+		this.artikel = artikel;
+	}
+	public String getKategori() {
+		return kategori;
+	}
+	public void setKategori(String kategori) {
+		this.kategori = kategori;
+	}
+	public String getTipe() {
+		return tipe;
+	}
+	public void setTipe(String tipe) {
+		this.tipe = tipe;
+	}
+	public String getNama_barang() {
+		return nama_barang;
+	}
+	public void setNama_barang(String nama_barang) {
+		this.nama_barang = nama_barang;
+	}
+	public double getKuantitas() {
+		return kuantitas;
+	}
+	public void setKuantitas(double kuantitas) {
+		this.kuantitas = kuantitas;
+	}
+	public String getUkuran() {
+		return ukuran;
+	}
+	public void setUkuran(String ukuran) {
+		this.ukuran = ukuran;
+	}
+	public double getHpp() {
+		return hpp;
+	}
+	public void setHpp(double hpp) {
+		this.hpp = hpp;
+	}
+	public double getHarga_jual() {
+		return harga_jual;
+	}
+	public void setHarga_jual(double harga_jual) {
+		this.harga_jual = harga_jual;
+	}
+	public double getTotal_hpp() {
+		return total_hpp;
+	}
+	public void setTotal_hpp(double total_hpp) {
+		this.total_hpp = total_hpp;
+	}
+	public String getKeterangan() {
+		return keterangan;
+	}
+	public void setKeterangan(String keterangan) {
+		this.keterangan = keterangan;
+	}
+	public int getRowstatus() {
+		return rowstatus;
+	}
+	public void setRowstatus(int rowstatus) {
+		this.rowstatus = rowstatus;
+	}
+	@Override
+	public String toString() {
+		return "PenyimpananKeluar [id=" + id + ","
+				+ " tanggal_transaksi=" + tanggal_transaksi + ", "
+				+ " id_store=" + id_store + ", "
+				+ " lokasi_store=" + lokasi_store + ", "
+				+ " artikel=" + artikel + ", "
+				+ "kategori=" + kategori + ","
+				+ " tipe=" + tipe + ","
+	            + " nama_barang=" + nama_barang + ","
+	            + " kuantitas=" + kuantitas + ","
+	            + " ukuran=" + ukuran + ","
+	            + " hpp=" + hpp + ","
+	            + " harga_jual=" + harga_jual + ","
+	            + " total_hpp=" + total_hpp + ","
+	            + " keterangan=" + keterangan + ","
+	            + " rowstatus=" + rowstatus + "]";
+	}
+}
