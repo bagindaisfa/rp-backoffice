388f76ad8cf1f4fbe570c9f5cd79d8222af70be0
bagindaisfa <58410256+bagindaisfa@users.noreply.github.com>
2022-05-23 17:52:37 +0700

    Add generated War (for deploying)

--- rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/RpbackofficeApplication.java
+++ rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/RpbackofficeApplication.java
@@ -2,8 +2,9 @@ package com.gbsystem.rpbackoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class RpbackofficeApplication {
+public class RpbackofficeApplication extends SpringBootServletInitializer {
 	
 	
 	public static void main(String[] args) {
