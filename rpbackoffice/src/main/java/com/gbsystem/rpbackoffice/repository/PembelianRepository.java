35369dc57708c9ae0bc8e53c20ee636c7965d35f
bagindaisfa <58410256+bagindaisfa@users.noreply.github.com>
2022-06-13 20:01:27 +0700

    Update pembelian repo

--- rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/repository/PembelianRepository.java
+++ rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/repository/PembelianRepository.java
@@ -14,7 +14,7 @@ public interface PembelianRepository extends JpaRepository<Pembelian, Long> {
 	List<Pembelian> findByRowstatus(@Param("rowstatus") int rowstatus);
 	
 	@Query(value = "SELECT * FROM pembelian WHERE rowstatus = 1 AND "
-			+ "( artikel LIKE %:keyword% OR tipe LIKE %:keyword% OR kategori LIKE %:keyword% OR "
+			+ "(pembelian_code LIKE %:keyword% OR artikel LIKE %:keyword% OR tipe LIKE %:keyword% OR kategori LIKE %:keyword% OR "
 			+ "nama_barang LIKE %:keyword% OR ukuran LIKE %:keyword% OR sku_code LIKE %:keyword% )", nativeQuery = true)
 	List<Pembelian> search(String keyword);
 	
