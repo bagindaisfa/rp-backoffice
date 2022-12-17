a6442179ed5830f00cbe8367c40999420ac351c1
bagindaisfa <bagindaisfa05@gmail.com>
2022-12-17 20:50:04 +0700

    Update be

--- rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/controllers/MasterProductController.java
+++ rpbackoffice/src/main/java/com/gbsystem/rpbackoffice/controllers/MasterProductController.java
@@ -234,6 +234,65 @@ public class MasterProductController {
     	    			}
     	    		}
     	    		
+        		} else {
+    	        	r.setNama_product(formatter.formatCellValue(row.getCell(1)));
+    	        	r.setType(Integer.valueOf(formatter.formatCellValue(row.getCell(2))));
+    	        	r.setType_name(formatter.formatCellValue(row.getCell(3)));
+    	        	r.setKategori(formatter.formatCellValue(row.getCell(4)));
+    	        	r.setNama_kategori(formatter.formatCellValue(row.getCell(5)));
+    	        	r.setArtikel_frame(formatter.formatCellValue(row.getCell(6)));
+    	        	r.setArtikel_lens(formatter.formatCellValue(row.getCell(7)));
+    	        	r.setUkuran(formatter.formatCellValue(row.getCell(8)));
+    	        	r.setKuantitas(Double.valueOf(formatter.formatCellValue(row.getCell(9))));
+    	        	r.setHpp(Double.valueOf(formatter.formatCellValue(row.getCell(10))));
+    	        	r.setHarga_jual(Double.valueOf(formatter.formatCellValue(row.getCell(11))));
+    	        	r.setRemarks(formatter.formatCellValue(row.getCell(12)));
+    	        	r.setRowstatus(1);
+    	        	eRepo.save(r);
+    	        	
+    	        	q = eStockRepo.findById_officeAndArtikel(1, formatter.formatCellValue(row.getCell(0)));
+    	        	q.setId_office(1);
+    	    		q.setLokasi_office("Kantor Pusat");
+    	    		q.setArtikel(formatter.formatCellValue(row.getCell(0)));
+    	    		q.setKategori(formatter.formatCellValue(row.getCell(4)));
+    	    		q.setNama_kategori(formatter.formatCellValue(row.getCell(5)));
+    	    		q.setType(Integer.valueOf(formatter.formatCellValue(row.getCell(2))));
+    	    		q.setType_name(formatter.formatCellValue(row.getCell(3)));
+    	    		q.setNama_barang(formatter.formatCellValue(row.getCell(1)));
+    	    		q.setUkuran(formatter.formatCellValue(row.getCell(8)));
+    	    		q.setSku_code(formatter.formatCellValue(row.getCell(13)));
+    	    		q.setHpp(Double.valueOf(formatter.formatCellValue(row.getCell(10))));
+    	    		q.setHarga_jual(Double.valueOf(formatter.formatCellValue(row.getCell(11))));
+    	    		q.setKuantitas(q.getKuantitas());
+    	    		q.setRowstatus(1);
+    	    		
+    	    		if (Double.valueOf(formatter.formatCellValue(row.getCell(9))) != null) {
+    	    			if (Double.valueOf(formatter.formatCellValue(row.getCell(9))) > 0) {
+        	    			q.setKuantitas(Double.valueOf(formatter.formatCellValue(row.getCell(9))));
+        	    			
+    	    				PenyimpananMasuk f = new PenyimpananMasuk();
+    	    				f.setId_office(1);
+    	    				f.setLokasi_office("Kantor Pusat");
+    	    				f.setPenerimaan_code("PS-" + new SimpleDateFormat("yyMM").format(new Date()) +"-"+ (ePenerimaanSuppRepo.count()+1));
+    	    				f.setTanggal_masuk(new Date());
+    	    				f.setArtikel(formatter.formatCellValue(row.getCell(0)));
+    	    	    		f.setKategori(formatter.formatCellValue(row.getCell(4)));
+    	    	    		f.setNama_kategori(formatter.formatCellValue(row.getCell(5)));
+    	    	    		f.setType(Integer.valueOf(formatter.formatCellValue(row.getCell(2))));
+    	    	    		f.setType_name(formatter.formatCellValue(row.getCell(3)));
+    	    	    		f.setNama_barang(formatter.formatCellValue(row.getCell(1)));
+    	    	    		f.setKuantitas(Double.valueOf(formatter.formatCellValue(row.getCell(9))));
+    	    	    		f.setUkuran(formatter.formatCellValue(row.getCell(8)));
+    	    	    		f.setSku_code(formatter.formatCellValue(row.getCell(13)));
+    	    	    		f.setHpp(Double.valueOf(formatter.formatCellValue(row.getCell(10))));
+    	    	    		f.setHarga_jual(Double.valueOf(formatter.formatCellValue(row.getCell(11))));
+    	    				f.setKeterangan("Barang Masuk Dari Master Product");
+    	    				f.setRowstatus(1);
+    	    				ePenyimpananRepo.save(f);
+    	    			}
+    	    		}
+
+    	    		eStockRepo.save(q);
         		}
     		}
     		
